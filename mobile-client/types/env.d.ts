b897f95727ecfd18b5ff4e0134ddd596bf3d92b9
pesho <peshooomanov@gmail.com>
2024-11-22 00:28:25 +0200

    client: FCM init

--- mobile-client/types/env.d.ts
+++ mobile-client/types/env.d.ts
@@ -3,4 +3,11 @@ declare module "@env" {
   export const REGISTER_ENDPOINT: string;
   export const LOGIN_ENDPOINT: string;
   export const GET_AUTHENTICATED_USER: string;
+  export const FIREBASE_APP_API_KEY: string;
+  export const FIREBASE_APP_AUTH_DOMAIN: string;
+  export const FIREBASE_APP_PROJECT_ID: string;
+  export const FIREBASE_APP_STORAGE_BUCKET: string;
+  export const FIREBASE_APP_MESSAGING_SENDER_ID: string;
+  export const FIREBASE_APP_APP_ID: string;
+  export const FIREBASE_APP_MEASUREMENT_ID: string;
 }
